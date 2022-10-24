5173c2ec74464b0e15efd9f510b29c8da5e14006
Evgenii Troinov <troinof@gmail.com>
2022-10-24 21:42:07 +0200

    feat(master): add '136. Single Number' task solution

---
+++ src/136_SingleNumber/solution.ts
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function singleNumber(nums: number[]): number {
+  let result = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    let counter = 1;
+
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] === nums[j]) {
+        counter++;
+      }
+    }
+
+    if (counter === 1) {
+      result = nums[i];
+    }
+  }
+
+  return result;
+}
